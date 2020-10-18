cab12ed124b19840cd0e53f4ef54bf0389023a58
Margarita Kitic <rikitau@protonmail.com>
2020-10-18 15:27:32 +0200

    erase mnemonic from the screen, ask for bip39 password

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // use std::{env, process};
 use std::str::FromStr;
+use std::io;
 
 // rand crate recommend using ring for secure applications
 use ring::rand::{SystemRandom,SecureRandom};
@@ -11,25 +12,40 @@ use bitcoin::util::bip32::ExtendedPrivKey;
 use bitcoin::util::bip32::ExtendedPubKey;
 use bitcoin::util::bip32::DerivationPath;
 
+use ansi_escapes::EraseLines;
+
 fn main() {
     let rng = SystemRandom::new();
     let mut entropy = vec![0u8; 16];
     rng.fill(&mut entropy).unwrap();
     let mnemonic = Mnemonic::from_entropy(&entropy).unwrap();
-    println!("Here is your mnemonic:\n{}", mnemonic);
-    // empty password for now
-    let seed = mnemonic.to_seed("");
+    println!("Here is your mnemonic:\n\n{}\n", mnemonic);
+    println!("Write it down and enter your BIP39 password, or just hit ENTER.");
+    println!("WARNING: mnemonic will be erased from the screen!");
+    let mut passwd = String::new();
+    io::stdin().read_line(&mut passwd).unwrap();
+    // remove trailing \n
+    passwd.truncate(passwd.len() - 1);
+    // delete mnemonic phrase, will work even if term window is small
+    // and mnemonic is displayed on two lines
+    print!("{}", EraseLines(8));
+
+    // generate seed and root key
+    let seed = mnemonic.to_seed(&passwd);
     let network = Network::Bitcoin;
     let root = ExtendedPrivKey::new_master(network, &seed).unwrap();
-    println!("Root key: {}", root);
 
     // we need secp256k1 context for key derivation
     let secp = Secp256k1::new();
 
     // derive child xpub
-    let path = DerivationPath::from_str("m/84h/0h/0h").unwrap();
+    // TODO: calculate fingerprint and display in Core-like format:
+    // [fingerprint/derivation]xpub
+    let path = DerivationPath::from_str("m/44h/0h/0h").unwrap();
     let child = root.derive_priv(&secp, &path).unwrap();
-    println!("Child at {}: {}", path, child);
     let xpub = ExtendedPubKey::from_private(&secp, &child);
-    println!("Public key at {}: {}", path, xpub);
+    println!("Child public key at {}: {}", path, xpub);
+
+    // TODO: derive keys for different purposes and construct
+    // Bitcoin Core descriptors
 }
